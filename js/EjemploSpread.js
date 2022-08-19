5587ab66b09635d3b1bb9e59201adc25d2a955e2
Cristian <79064096+Cr1ss7@users.noreply.github.com>
2022-08-19 07:25:15 -0600

    Add files via upload

--- js/EjemploSpread.js
+++ js/EjemploSpread.js
@@ -4,14 +4,14 @@ function suma (){
     var resutado = 0;
     var num = [4];
     for(i=0; i<4; i++){
-        num[i] = prompt("Digite un numero:");
+        num[i] = prompt("Digite 4 numeros que usted quiera sumar:");
     }
     resutado = resultado(...num);
-    alert("El resultado de la suma, resta y multiplicacion es :"+ resutado);
+    alert("El resultado de la suma de 4 digitos es :"+ resutado);
 }
 
 function resultado (w,x,y,z){
-    return parseInt(w) + parseInt(x) - parseInt(y) * parseInt(z);
+    return parseInt(w) + parseInt(x) + parseInt(y) + parseInt(z);
 }
 
 
